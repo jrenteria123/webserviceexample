11cb306151d2baa0a011cbf92fe3ea8a269581cf
Mike Litman <awesomefat@gmail.com>
2015-11-14 10:49:13 -0600

    initial commit

--- Webservice Example/ViewController.swift
+++ Webservice Example/ViewController.swift
@@ -10,8 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        let manager = AFHTTPRequestOperationManager()
+        manager.GET("http://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=json&units=metric&cnt=1&appid=553cfbc038f1133fa59243a9a900358a", parameters: nil,
+            success:
+            { (operation: AFHTTPRequestOperation, responseObject: AnyObject) -> Void in
+                print("Response: " + responseObject.description)
+            },
+            failure:
+            { (operation: AFHTTPRequestOperation?, error: NSError) -> Void in
+                print("Error: " + error.localizedDescription)
+            })
         // Do any additional setup after loading the view, typically from a nib.
     }
 
---
+++ bridge.h
@@ -0,0 +1,13 @@
+//
+//  bridge.h
+//  Webservice Example
+//
+//  Created by Michael Litman on 11/14/15.
+//  Copyright © 2015 Michael Litman. All rights reserved.
+//
+#import "AFNetworking.h"
+#ifndef bridge_h
+#define bridge_h
+
+
+#endif /* bridge_h */
